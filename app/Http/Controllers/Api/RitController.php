b8a80d0136a8f27ee0d26c1cb0713727b9ca3e49
akristianto03 <ezralfredo@gmail.com>
2025-02-01 00:30:39 +0700

    update

--- app/Http/Controllers/Api/RitController.php
+++ app/Http/Controllers/Api/RitController.php
@@ -91,11 +91,11 @@ class RitController extends Controller
     public function get_owner_stock()
     {
         $rits = Rit::whereNull("sold_date")
-            ->where(function ($query) {
-                $query->whereNull("tonnage_left")
-                    ->orWhere("tonnage_left", ">", 0)
-                    ->orWhere("tonnage_left", "<", 0);
-            })
+            // ->where(function ($query) {
+            //     $query->whereNull("tonnage_left")
+            //         ->orWhere("tonnage_left", ">", 0)
+            //         ->orWhere("tonnage_left", "<", 0);
+            // })
             ->get();
 
         $return = [
